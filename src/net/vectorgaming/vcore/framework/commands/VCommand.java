d826adb4d808b8ace1289243cc06d0538ef45275
Kenny Williams <kenny@vectorgaming.net>
2013-09-20 21:11:04 -0700

    Added javadocs

--- src/net/vectorgaming/vcore/framework/commands/VCommand.java
+++ src/net/vectorgaming/vcore/framework/commands/VCommand.java
@@ -20,12 +20,23 @@ public abstract class VCommand extends Command
     private VertexPlugin plugin;
     private ArrayList<SubCommand> subCommands = new ArrayList<>();
     
+    /**
+     *
+     * @param command Name of the command
+     * @param plugin Plugin the command is coming from
+     */
     protected VCommand(String command, VertexPlugin plugin)
     {
         super(command);
         this.plugin = plugin;
     }
     
+    /**
+     *
+     * @param command Name of the command
+     * @param aliases A list of aliases for the command
+     * @param plugin Plugin the command is coming from
+     */
     protected VCommand(String command, List<String> aliases, VertexPlugin plugin)
     {
         super(command);
@@ -33,31 +44,125 @@ public abstract class VCommand extends Command
         this.setAliases(aliases);
     }
         
+    /**
+     * Called whenever the command is ran
+     * @param cs Sender of the command
+     * @param args Arguments after the command
+     */
     public abstract void run(CommandSender cs, String[] args);
+    
+    /**
+     * Returns the description of the command and what it does.
+     * @return Command description
+     */
     public abstract String getDescription();
+    
+    /**
+     * Returns a String stating how the command should be properly used
+     * @return Command usage
+     */
     public abstract String getUsage();
+    
+    /**
+     * Returns the permission required for a player to use the command
+     * @return Permission node for the command
+     */
     public abstract String getPermission();
+    
+    /**
+     * Returns the minimum amount of arguments required for the command to be executed
+     * @return Minimum amount of arguments
+     */
     public abstract Integer getMinArgsLength();
+    
+    /**
+     * Checks to see if the command can only be executed by a player
+     * @return True if can only be used by a player
+     */
     public abstract boolean isPlayerOnlyCommand();
     
-    public ArrayList<SubCommand> getSubCommands() {return subCommands;}
+    /**
+     * Returns a list of sub commands that the command has registered. 
+     * 
+     * This list should be empty if the command has no sub commands
+     * @return A list of sub commands
+     */
+    public ArrayList<SubCommand> getSubCommands() 
+    {
+        return subCommands;
+    }
     
-    public void addSubCommand(SubCommand c) {subCommands.add(c);}
+    /**
+     * Adds a sub command to the super command
+     * @param c Sub command that should be added
+     */
+    public void addSubCommand(SubCommand c) 
+    {
+        subCommands.add(c);
+    }
     
-    public void removeSubCommand(SubCommand c) {subCommands.remove(c);}
+    /**
+     * Removes a sub command from the super command
+     * @param c Sub command that should be removed
+     */
+    public void removeSubCommand(SubCommand c) 
+    {
+        subCommands.remove(c);
+    }
     
-    public VertexPlugin getPlugin() {return plugin;}
+    /**
+     * Adds an alias to the command. This alias can be used as a substitute to execute the command
+     * @param alias Name of the alias
+     */
+    public void addAlias(String alias)
+    {
+        List<String> aliases = getAliases();
+        if(!aliases.contains(alias))
+        {
+            aliases.add(alias);
+        }
+        setAliases(aliases);
+    }
     
+    /**
+     * Returns the plugin that the command is being sent from
+     * @return Plugin sending the command
+     */
+    public VertexPlugin getPlugin() 
+    {
+        return plugin;
+    }
+    
+    /**
+     * Sends the sender a message prefixed by brackets surrounding the plugin's name
+     * @param cs Sender to send the message to
+     * @param message Message to send to the sender
+     */
     public void sendMessage(CommandSender cs, String message)
     {
         cs.sendMessage(ChatColor.BLACK+"["+VCoreAPI.getColorScheme().getPluginColor()+plugin.getName()+ChatColor.BLACK+"] "+ChatColor.WHITE+message);
     }
     
+    /**
+     * Sends the sender an error message using the default color scheme
+     * @param cs Sender to send the message to
+     * @param message Message to send the sender
+     */
     public void sendErrorMessage(CommandSender cs, String message)
     {
         cs.sendMessage(VCoreAPI.getColorScheme().getErrorColor()+"Error: "+message);
     }
     
+    /**
+     * This method executes the command going through all checks to ensure the sender can
+     * execute the command. 
+     * 
+     * The method should NOT be overriden.
+     * @param cs Sender of the command
+     * @param lbl Command name
+     * @param args Command arguments
+     * @return boolean value
+     */
     @Override
     public final boolean execute(CommandSender cs, String lbl, String[] args)
     {
