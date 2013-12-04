b44f266ca95042546f743e33c687a9626eb0c4cc
Kenny Williams <kenny@vectorgaming.net>
2013-12-03 19:32:03 -0800

    Removed getAPI method

--- src/net/vectorgaming/vcore/VCore.java
+++ src/net/vectorgaming/vcore/VCore.java
@@ -2,7 +2,6 @@ package net.vectorgaming.vcore;
 
 import net.vectorgaming.vcore.commands.TestCommand;
 import net.vectorgaming.vcore.framework.VertexPlugin;
-import net.vectorgaming.vcore.framework.VertexAPI;
 import net.vectorgaming.vcore.framework.commands.CommandManager;
 import org.bukkit.plugin.Plugin;
 
@@ -22,12 +21,6 @@ public class VCore extends VertexPlugin
 
     @Override
     public Plugin getPlugin() {return this;}
-
-    @Override
-    public VertexAPI getAPI()
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
 
     @Override
     public void onDisable(){}
